4aa6a2f0fa7978f9855a1981fda71d49b44bfdb6
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-27 23:59:57 +0530

    [SUNNY]Add.Welcome to Indian State Census Analyser Problem

---
+++ StateAnalyser/CensusDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace StateAnalyser
+{
+    class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int tin;
+        public string stateCode;
+        public long population;
+        public long area;
+        public long density;
+        public long housingUnits;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+
+        public CensusDTO(StateCodeDAO stateCodeDAO)
+        {
+            this.serialNumber = stateCodeDAO.serialNummber;
+            this.stateName = stateCodeDAO.stateName;
+            this.tin=stateCodeDAO.tin;
+            this.stateCode = stateCodeDAO.stateCode;
+        }
+    }
+}
